c9a25ad2ab29fdcf11944d1f6b56c89b296c56dd
escottalexander@gmail.com <escottalexander@gmail.com>
2023-03-01 21:42:47 -0500

    initial commit

---
+++ precache-manifest.e12990d9aead758f0b80c9b8a9600c0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea14332b56d1ee6276f3b6985558fbbe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "35c4c8a2ca1e48f148ce",
+    "url": "./static/css/2.10e48097.chunk.css"
+  },
+  {
+    "revision": "49f73b9fe1c8f0e0e1cb",
+    "url": "./static/css/main.4be7d347.chunk.css"
+  },
+  {
+    "revision": "35c4c8a2ca1e48f148ce",
+    "url": "./static/js/2.6123894b.chunk.js"
+  },
+  {
+    "revision": "9fd8b50da59301e0b15247e6ba3ebb90",
+    "url": "./static/js/2.6123894b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f73b9fe1c8f0e0e1cb",
+    "url": "./static/js/main.284032a2.chunk.js"
+  },
+  {
+    "revision": "028762adaf48b66839f6",
+    "url": "./static/js/runtime-main.08baa623.js"
+  },
+  {
+    "revision": "88e89622069ec10fefdfae76d9981820",
+    "url": "./static/media/Air Balloon - TTF.88e89622.ttf"
+  }
+]);
